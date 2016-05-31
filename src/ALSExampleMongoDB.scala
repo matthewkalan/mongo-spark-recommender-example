21dade381dc665c9b2add6e5755eecdbebb3e29f
matthewkalan <matthewkalan@gmail.com>
2016-05-31 14:31:09 -0500

    First version running successfully on Databricks

--- src/ALSExampleMongoDB.scala
+++ src/ALSExampleMongoDB.scala
@@ -16,7 +16,6 @@ import com.mongodb.spark.sql._
 import java.util.Calendar
 import java.text.SimpleDateFormat
 import java.util.Date
-//import java.util.concurrent.TimeUnit
 
 object ALSExampleMongoDB {
 
@@ -36,59 +35,61 @@ object ALSExampleMongoDB {
     // get current time at beginning and end to calculate job latency
     val formatter = new SimpleDateFormat("hh:mm:ss")
     val startTime = Calendar.getInstance().getTime()
-    println("Start time:" + formatter.format(startTime))
+    println("Start time: " + formatter.format(startTime))
     
-    val conf = new SparkConf()
-      .setMaster(args(2))
+    //this conf should only be used when run locally because sc.getOrCreate() is called below
+    val conf = new SparkConf()      
+      .setMaster("local")
       .setAppName("ALSExampleMongoDB")
-      .set("spark.mongodb.input.uri", args(1))
     val sc = SparkContext.getOrCreate(conf)
     val sqlContext = SQLContext.getOrCreate(sc)
     var ratings = sqlContext.emptyDataFrame    //because compiler needs definition if we exit early
-                
-    //read from given data source
-    val dsName = args(0).toLowerCase()
-    if (dsName == "mongodb") {
-      var inputUri = args(1)                                       //pass MongoDB connection string from args[]
+    var validInput = false
+    
+    //confirm 2 arguments of proper values
+    if (args.length == 0) {
+      println("# of args: " + args.length + "\nArgs expected: mongodb|file <mongodb connection string>|<file-location>")
+    } else if (args(0).toLowerCase() == "mongodb") {
+      var inputUri = args(1)                                       //pass MongoDB connection string from args
       println("inputUri = " + inputUri)
       ratings = sqlContext.read.option("uri", inputUri).mongo()
       ratings.printSchema()
-    } else if (dsName == "file" ) {
+      validInput = true
+    } else if (args(0).toLowerCase() == "file" ) {
       import sqlContext.implicits._
       ratings = sc.textFile(args(1))
-      .map(Rating.parseRating)
-      .toDF()
-    } else {
-      println("Args: mongodb|file <file-location>")
-      sys.exit(1)
+        .map(Rating.parseRating)
+        .toDF()
+      validInput = true
     }
 
-    val Array(training, test) = ratings.randomSplit(Array(0.8, 0.2))
-
-    // Build the recommendation model using ALS on the training data
-    val als = new ALS()
-      .setMaxIter(5)
-      .setRegParam(0.01)
-      .setUserCol("userId")
-      .setItemCol("movieId")
-      .setRatingCol("rating")
-    val model = als.fit(training)
-
-    // Evaluate the model by computing the RMSE on the test data
-    val predictions = model.transform(test)
-      .withColumn("rating", col("rating").cast(DoubleType))
-      .withColumn("prediction", col("prediction").cast(DoubleType))
-
-    val evaluator = new RegressionEvaluator()
-      .setMetricName("rmse")
-      .setLabelCol("rating")
-      .setPredictionCol("prediction")
-    val rmse = evaluator.evaluate(predictions)
-    println(s"Root-mean-square error = $rmse")
-    
-    val endTime = Calendar.getInstance().getTime()
-    var elapsedTime = (endTime.getTime() - startTime.getTime()) / 1000
-    println("End time:" + formatter.format(endTime) + ", Total time: " + elapsedTime + " seconds")
-    
+    if (validInput) {
+      val Array(training, test) = ratings.randomSplit(Array(0.8, 0.2))
+  
+      // Build the recommendation model using ALS on the training data
+      val als = new ALS()
+        .setMaxIter(5)
+        .setRegParam(0.01)
+        .setUserCol("userId")
+        .setItemCol("movieId")
+        .setRatingCol("rating")
+      val model = als.fit(training)
+  
+      // Evaluate the model by computing the RMSE on the test data
+      val predictions = model.transform(test)
+        .withColumn("rating", col("rating").cast(DoubleType))
+        .withColumn("prediction", col("prediction").cast(DoubleType))
+  
+      val evaluator = new RegressionEvaluator()
+        .setMetricName("rmse")
+        .setLabelCol("rating")
+        .setPredictionCol("prediction")
+      val rmse = evaluator.evaluate(predictions)
+      println(s"Root-mean-square error = $rmse")
+      
+      val endTime = Calendar.getInstance().getTime()
+      var elapsedTime = (endTime.getTime() - startTime.getTime()) / 1000
+      println("End time:" + formatter.format(endTime) + ", Total time: " + elapsedTime + " seconds")
+    } 
   }
 }
